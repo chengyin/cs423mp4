c81df3beac8ee5d8670ad745690df8997dee04eb
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-01 22:08:49 -0600

    A general job queue

---
+++ Android423MP4/src/task/JobQueue.java
@@ -0,0 +1,38 @@
+package task;
+
+import java.util.Collection;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class JobQueue {
+    LinkedBlockingQueue<Job> jobs; // Linked-list based
+    
+    /**
+     * Create a JobQueue with an empty queue
+     */
+    public JobQueue() {
+	super();
+	jobs = new LinkedBlockingQueue<Job>();
+    }
+    
+    /**
+     * Create a JobQueue with a collection of initial jobs
+     * @param jobs
+     */
+    public JobQueue(Collection<Job> jobs) {
+	super();
+	this.jobs = new LinkedBlockingQueue<Job>(jobs);	
+    }
+    
+    public void enqueue(Job job) {
+	try {
+	    this.jobs.put(job);
+	} catch (InterruptedException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+    
+    public Job dequeue() {
+	return this.jobs.poll();
+    }
+}
