bff778d27ae182b0c79006d2657d6cf2bec25a10
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-01 23:04:44 -0600

    Add results to wrap multiple result

---
+++ Android423MP4/src/task/Results.java
@@ -0,0 +1,11 @@
+package task;
+
+import java.util.ArrayList;
+
+public class Results {
+    protected ArrayList<Result> results;
+    
+    public void addResult(Result result) {
+	this.results.add(result);
+    }
+}
