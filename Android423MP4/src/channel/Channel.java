ef420cb5e9cb8640f101ccd5e27c434b2334248f
Rohan Sharma <rohan@rohan-netbook.(none)>
2011-12-02 05:23:58 -0600

    Alternative IP lookup

--- Android423MP4/src/channel/Channel.java
+++ Android423MP4/src/channel/Channel.java
@@ -3,7 +3,13 @@ package channel;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import android.util.Log;
 
 public abstract class Channel {
     Socket socket = null;
@@ -14,7 +20,9 @@ public abstract class Channel {
 	this.outStream.writeUTF(message);
     }
 
-    public String getMessage(String message) throws IOException {
+    public String getMessage() throws IOException {
+	if (this.inStream == null)
+	    return null;
 	return this.inStream.readUTF();
     }
 
@@ -22,8 +30,25 @@ public abstract class Channel {
 	return this.socket.getLocalPort();
     }
 
+    /**
+     * http://www.droidnova.com/get-the-ip-address-of-your-device,304.html
+     * @return
+     */
     public String getLocalIPAddress() {
-	return this.socket.getLocalAddress().getHostAddress();
+	    try {
+	        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+	            NetworkInterface intf = en.nextElement();
+	            for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+	                InetAddress inetAddress = enumIpAddr.nextElement();
+	                if (!inetAddress.isLoopbackAddress()) {
+	                    return inetAddress.getHostAddress().toString();
+	                }
+	            }
+	        }
+	    } catch (SocketException ex) {
+	        Log.e("", ex.toString());
+	    }
+	    return null;
     }
 
     public int getRemotePort() {
