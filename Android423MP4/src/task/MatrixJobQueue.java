32ce17e2c26d420668db104597baee3bd1af28ee
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-01 22:34:56 -0600

    Job queue for MatrixJob

---
+++ Android423MP4/src/task/MatrixJobQueue.java
@@ -0,0 +1,41 @@
+package task;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import matrix.Matrix;
+
+public class MatrixJobQueue extends JobQueue {
+    public static MatrixJobQueue generateJobQueueWithMatrixs(Matrix matrix1,
+	    Matrix matrix2) {
+	if (matrix1.getRow() != matrix2.getRow()
+		|| matrix1.getCol() != matrix2.getCol()) {
+	    return null;
+	}
+
+	int row = matrix1.getRow(), col = matrix1.getCol();
+	MatrixJob[] jobs = new MatrixJob[row * col];
+
+	for (int r = 0; r < row; r++) {
+	    for (int c = 0; c < col; c++) {
+		jobs[r * c] = new MatrixJob(r, c, matrix1.getElement(r, c), matrix2.getElement(r, c));
+	    }
+	}
+	
+	return (new MatrixJobQueue(Arrays.asList(jobs)));
+    }
+
+    public MatrixJobQueue(Collection<MatrixJob> jobs) {
+	super();
+	this.jobs = new LinkedBlockingQueue<Job>(jobs);
+    }
+    
+    public MatrixJob dequeue() {
+	return (MatrixJob) super.dequeue();
+    }
+    
+    public void enqueue(MatrixJob job) {
+	super.enqueue(job);
+    }
+}
