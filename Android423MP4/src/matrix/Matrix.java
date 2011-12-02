197655e175605b65778ed83eafe6c0edfef50fa3
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-01 22:34:36 -0600

    Better wrapping

--- Android423MP4/src/matrix/Matrix.java
+++ Android423MP4/src/matrix/Matrix.java
@@ -2,8 +2,12 @@ package matrix;
 
 public class Matrix {
     private int[][] data;
+    private int row;
+    private int col;
     
     public Matrix(int row, int col, int value) {
+	this.row = row;
+	this.col = col;
 	this.data = new int[row][col];
 	
 	for (int r = 0; r < row; r++) {
@@ -11,5 +15,21 @@ public class Matrix {
 		this.data[r][c] = value;
 	    }
 	}
+    }
+    
+    public int getElement(int row, int col) {
+	return this.data[row][col];
+    }
+    
+    public int[][] getData() {
+        return data;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
     }
 }
