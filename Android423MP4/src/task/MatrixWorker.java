612ad1560572e1b6740c189746884f821a4dcf50
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-01 23:03:08 -0600

    Fixing inherit problem

--- Android423MP4/src/task/MatrixWorker.java
+++ Android423MP4/src/task/MatrixWorker.java
@@ -1,7 +1,24 @@
 package task;
 
-public class MatrixWorker {
+import control.HardwareMonitor;
+
+public class MatrixWorker extends Worker{
+    protected MatrixResults results;
+
+    public MatrixWorker(int id, HardwareMonitor hwMonitor) {
+	super(id, hwMonitor);
+    }
+    
+    public MatrixWorker(int id, HardwareMonitor hwMonitor, Results results) {
+	super(id, hwMonitor, results);
+    }
+
     public MatrixResult processJob(MatrixJob job) {
 	return new MatrixResult(job.getRow(), job.getCol(), job.getM1() + job.getM2());
+    }
+
+    @Override
+    public Result processJob(Job job) {
+	return this.processJob((MatrixJob) job);
     }
 }
