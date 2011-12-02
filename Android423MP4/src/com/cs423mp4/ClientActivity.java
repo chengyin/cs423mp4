93d282dd2303c2787eaa0ed1d0b75e19218a2e84
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-02 03:47:42 -0600

    Add Client Acitivity and view

--- Android423MP4/src/com/cs423mp4/ClientActivity.java
+++ Android423MP4/src/com/cs423mp4/ClientActivity.java
@@ -1,15 +1,37 @@
 package com.cs423mp4;
 
+import control.HardwareMonitor;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
 
-public class ClientActivity extends Activity {
+public class ClientActivity extends MonitorActivity {
+    private String server_IP;
+    private int server_port;
+
+    private HardwareMonitor monitor = new HardwareMonitor();
+    private TextView cpuUsageView;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
+	super.onCreate(savedInstanceState);
+	this.setContentView(R.layout.client);
+
+	Bundle extras = getIntent().getExtras();
+
+	this.server_IP = extras.getString("ip");
+	this.server_port = extras.getInt("port");
+
+	this.cpuUsageView = (TextView) findViewById(R.id.usageView);
+
+	this.init();
     }
 
+    private void init() {
+	this.displayCPUUsage(this.cpuUsageView, this.monitor);
+    }
 }
