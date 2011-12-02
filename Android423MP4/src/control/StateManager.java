579024feebacecdc683d7a7c8f8e5be1d0100302
Rohan Sharma <rohan@rohan-netbook.(none)>
2011-12-02 01:06:47 -0600

    Stubs for the state manager

--- Android423MP4/src/control/StateManager.java
+++ Android423MP4/src/control/StateManager.java
@@ -2,6 +2,17 @@ package control;
 
 public class StateManager {
     public StateManager() {
-	
+    }
+    
+    public int getRemoteQueueSize() {
+	return 0;
+    }
+    
+    public double getRemoteThrottlingValue() {
+	return 0.0;
+    }
+    
+    public int getRemoteCPUUsage() {
+	return 0;
     }
 }
