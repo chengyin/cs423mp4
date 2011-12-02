bdcfa562eb208600107ff4c1df5cc3ebb0fe2bea
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-02 03:01:38 -0600

    Add ClientActivity (file)

---
+++ Android423MP4/src/com/cs423mp4/ClientActivity.java
@@ -0,0 +1,15 @@
+package com.cs423mp4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClientActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+}
