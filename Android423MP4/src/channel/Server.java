39674eacb18a1d2845dd0e92752fb0d156f5332c
Rohan Sharma <rohan@rohan-netbook.(none)>
2011-12-03 00:16:15 -0600

    Back to threaded listen.

--- Android423MP4/src/channel/Server.java
+++ Android423MP4/src/channel/Server.java
@@ -10,33 +10,37 @@ import android.util.Log;
 public class Server extends Channel {
     int port;
     ServerSocket serverSocket;
-    
+
     public Server(int port) throws IOException {
 	super();
 	this.port = port;
 	this.serverSocket = new ServerSocket(port);
-	/*
+
 	Runnable listener = new Runnable() {
 	    public void run() {
-		try { */
-	//listen();
-	/*	} catch (IOException e) {
+		try {
+		    listen();
+		} catch (IOException e) {
 		}
 	    }
-	};	
-	new Thread(listener).start();*/
+	};
+	new Thread(listener).start();
     }
-    
+
+    public boolean isConnected() {
+	return this.socket != null;
+    }
+
     public void listen() throws IOException {
 	this.socket = this.serverSocket.accept();
 	this.inStream = new DataInputStream(this.socket.getInputStream());
 	this.outStream = new DataOutputStream(this.socket.getOutputStream());
     }
-    
+
     public int getLocalPort() {
 	return this.serverSocket.getLocalPort();
     }
-    
+
     protected void finalize() {
 	if (this.socket != null) {
 	    try {
@@ -46,7 +50,7 @@ public class Server extends Channel {
 		e.printStackTrace();
 	    }
 	}
-	
+
 	if (this.inStream != null) {
 	    try {
 		this.inStream.close();
@@ -55,7 +59,7 @@ public class Server extends Channel {
 		e.printStackTrace();
 	    }
 	}
-	
+
 	if (this.outStream != null) {
 	    try {
 		this.outStream.close();
