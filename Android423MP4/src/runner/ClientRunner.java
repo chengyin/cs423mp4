4af44187f935bd71456f661e30d5fd8240967438
Chengyin Liu <chengyin.liu@gmail.com>
2011-12-03 05:18:35 -0600

    Better inheritance

--- Android423MP4/src/runner/ClientRunner.java
+++ Android423MP4/src/runner/ClientRunner.java
@@ -6,17 +6,25 @@ import java.net.UnknownHostException;
 import channel.Client;
 
 public class ClientRunner extends MonitorRunner {
-    Client channel;
+    private Client channel;
 
     public ClientRunner(String serverIP, int serverPort) {
 	try {
-	    this.channel = new Client(serverIP, serverPort);
+	    this.setChannel(new Client(serverIP, serverPort));
 	} catch (UnknownHostException e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	} catch (IOException e) {
 	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	    e.printStackTrace(	);
 	}
+    }
+
+    public Client getChannel() {
+	return channel;
+    }
+
+    public void setChannel(Client channel) {
+	this.channel = channel;
     }
 }
