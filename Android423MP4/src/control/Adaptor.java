cecb23c7db338113d1651b354e95f689325e91ff
unknown <binbian9@binbian9-PC.(none)>
2011-12-05 03:26:26 -0600

    Compute function

--- Android423MP4/src/control/Adaptor.java
+++ Android423MP4/src/control/Adaptor.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import android.os.Handler;
 import android.os.Message;
 
+import task.Job;
 import task.JobQueue;
 
 public class Adaptor {
@@ -24,7 +25,45 @@ public class Adaptor {
     }
     public void Compute()
     {
+	double s1 = stateManager.getRemoteState().getHwMonitor().getCPUUsage();
+	double s2 = monitor.getCPUUsage();
+	double j1 = stateManager.getRemoteState().getJobQueueRemaining();
+	double j2 = stateManager.getLocalState().getJobQueueRemaining();
 	
+	int x = (int) ((s1*j1-s2*j2)/(s1+s2));
+	
+	if(Math.abs(x/j1) > 0.25)
+	{
+	    String message = ""+x;
+	    transferManager.sendRequest(message);
+	    String control_message = transferManager.receiveRequest();
+	    String[] inf = control_message.split("");
+	    if(x>0)//local need to do more jobs
+	    {
+		for(int i=0;i<inf.length;i=i+2)
+		{
+		    Job job = new Job();
+		    job.x = inf[i];
+		    job.y = inf[i+1];
+		    try {
+			localJobQueue.enqueue(job);
+		    } catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    }
+		}
+	    }
+	    else
+	    {
+		for(int i=0;i<inf.length;i=i+2)
+		{
+		    Job job = new Job();
+		    job.x = inf[i];
+		    job.y = inf[i+1];
+		    localJobQueue.deljob(job);
+		}
+	    }
+	}
     }
     
     public Runnable inputListener = new Runnable() {
